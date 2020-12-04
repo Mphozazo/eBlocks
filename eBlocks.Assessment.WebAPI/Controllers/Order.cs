d3e9e8624a0c7cc28a066c4a1e19decace88a7b0
Walter Heisi <Mphozazo@gmail.com>
2020-12-04 09:25:48 +0200

    Order controller added

---
+++ eBlocks.Assessment.WebAPI/Controllers/Order.cs
@@ -0,0 +1,15 @@
+﻿using eBlocks.Assessment.Models;
+using eBlocks.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eBlocks.Assessment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Base<OrderDetails, OrderDetailRepo>
+    {
+        public OrderController(OrderDetailRepo orderRepository) : base(orderRepository)
+        {
+        }
+    }
+}
